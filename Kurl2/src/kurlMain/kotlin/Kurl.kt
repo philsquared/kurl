83704afa0edd86c1f650270c03028abbf006fc24
Phil nash <github@philnash.me>
2019-10-02 21:23:38 +0100

    First step: - early exit - set user agent - update url - write function and write data, but commented out

--- Kurl2/src/kurlMain/kotlin/Kurl.kt
+++ Kurl2/src/kurlMain/kotlin/Kurl.kt
@@ -1,18 +1,27 @@
 import interop.*
 import kotlinx.cinterop.*
+import kotlin.system.exitProcess
 
 fun main()
 {
   val curl = curl_easy_init()
-  if(curl != null) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/libcurl/c/simple.html")
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
+  if(curl == null)
+    exitProcess(-1)
 
-    val res = curl_easy_perform(curl)
 
-    if(res != CURLE_OK)
-        println("curl_easy_perform() failed: ${curl_easy_strerror(res)?.toKString()}")
+  curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+  curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/libcurl/c/getinmemory.html")
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
 
-    curl_easy_cleanup(curl)
-  }
+//  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+//  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+
+  val res = curl_easy_perform(curl)
+
+  if(res != CURLE_OK)
+      println("curl_easy_perform() failed: ${curl_easy_strerror(res)?.toKString()}")
+
+  curl_easy_cleanup(curl)
+
 }
