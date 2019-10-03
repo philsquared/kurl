7192317ba4a691fe960bfb2b21af1cf03e001a63
Phil nash <giuthub@philnash.me>
2019-10-03 10:47:21 +0100

    Minor clean-up

--- Curl1/main.c
+++ Curl1/main.c
@@ -10,13 +10,13 @@ int main(void)
 
     curl = curl_easy_init();
     if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/libcurl/c/simple.html");
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/libcurl/c/simple.html");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                  curl_easy_strerror(res));
         curl_easy_cleanup(curl);
     }
     return 0;
